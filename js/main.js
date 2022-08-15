85c6ad5fdd52413662b5ed27de86c4865be31dcb
Andre Maeland <andre.maeland@netpower.no>
2022-08-15 10:50:54 +0200

    refact

--- js/main.js
+++ js/main.js
@@ -1,75 +1,5 @@
 // variables
-const buttons = document.querySelectorAll('.btn');
-const container = document.querySelector('.container');
-const upDtxt = {
-    daily: 'Yesterday',
-    weekly: 'Last Week',
-    monthly: 'Last Month',
-};
-
-let timeframe = 'weekly';
-let secBox;
-
-let edata = {};
-
-// eventlistener
-
-buttons.forEach((element) => {
-    element.addEventListener('click', buttonActive);
-});
-
-// json data
-
-fetch('./js/data.json')
-    .then((resp) => resp.json())
-    .then((data) => {
-        data.forEach((element) => {
-            container.insertAdjacentHTML(
-                'beforeend',
-                renderData(element, timeframe)
-            );
-        });
-        data.forEach((element) => {
-            edata[element.title] = element.timeframes;
-        });
-        secBox = document.querySelectorAll('.secondary-box');
-    });
-
-// functions
-
-function buttonActive(event) {
-    buttons.forEach((element) => {
-        element.classList.remove('active');
-    });
-    event.target.classList.add('active');
-    timeframe = event.target.innerText.toLowerCase();
-    updateBoxes(timeframe);
-}
-
-function updateBoxes(timeframe) {
-    secBox.forEach((box) => {
-        updateBox(box, timeframe);
-    });
-}
-
-function updateBox(box, timeframe) {
-    const title = box.querySelector('.stats-box-h2').innerText;
-    const current = edata[title][timeframe].current;
-    const previous = edata[title][timeframe].previous;
-
-    const hoursEl = box.querySelector('.stats-box-hours-p');
-    hoursEl.innerText = `${current}hrs`;
-    const msgEl = box.querySelector('.stats-box-des-p');
-    msgEl.innerText = `${upDtxt[timeframe]} - ${previous}hrs`;
-}
-
-function renderData(element, timeframe) {
-    let title = element.title;
-    let current = element.timeframes[timeframe].current;
-    let previous = element.timeframes[timeframe].previous;
-
-    return `
-<div class="secondary-box ${title.toLowerCase().replace(' ', '-')}">
+/* <div class="secondary-box ${title.toLowerCase().replace(' ', '-')}">
     <div class="stats-box">
         <div class="col">
             <div class="col-h2">
@@ -92,6 +22,119 @@ function renderData(element, timeframe) {
             </div>
         </div>
     </div>
-</div>
-`;
+</div> */
+
+const buttons = document.querySelectorAll('.btn')
+const container = document.querySelector('.container')
+
+const upDtxt = {
+    daily: 'Yesterday',
+    weekly: 'Last Week',
+    monthly: 'Last Month',
+}
+
+let timeframe = 'weekly'
+let secBox
+
+let edata = {}
+
+buttons.forEach((element) => {
+    element.addEventListener('click', buttonActive)
+})
+
+function createCard(task, time) {
+    const bg = document.createElement('div');
+    bg.classList.add('secondary-box', task.replace(' ', '-'))
+    const statsBoxDiv = document.createElement('div');
+    statsBoxDiv.classList.add('stats-box');
+    const colDiv = document.createElement('div');
+    colDiv.classList.add('col');
+    const title = document.createElement('h2');
+    title.classList.add('stats-box-h2');
+    title.innerText = task
+    const col2Div = document.createElement('div');
+    col2Div.classList.add('col-2');
+    const statsBoxHoursDiv = document.createElement('div')
+    statsBoxHoursDiv.classList.add('stats-box-hours')
+    const statsBoxHoursP = document.createElement('p');
+    statsBoxHoursP.innerText = time[timeframe].current
+    const statsBoxDescriptionDiv = document.createElement('div')
+    statsBoxDescriptionDiv.classList.add('stats-box-des')
+    const statsBoxDescriptionP = document.createElement('p')
+    statsBoxDescriptionP.classList.add('stats-box-des-p')
+    statsBoxDescriptionP.innerText = time[timeframe].previous
+ 
+    colDiv.append(title)
+    statsBoxDescriptionDiv.append(statsBoxDescriptionP)
+    statsBoxHoursDiv.append(statsBoxHoursP)
+    col2Div.append(statsBoxHoursDiv, statsBoxDescriptionDiv)
+    statsBoxDiv.append(colDiv,col2Div)
+    
+    return statsBoxDiv
+
+}
+fetchAndRender()
+
+async function fetchAndRender() {
+    const tasks = await getTasksByTime()
+    
+    for (const [task, time] of Object.entries(tasks)) {
+        console.log(task,time)
+        container.append(createCard(task,time))
+    }
+
+}
+
+async function getTasksByTime() {
+    const tasksByTime = {}
+
+    const tasks = await fetchTasks()
+
+    tasks.forEach(task => {
+        tasksByTime[task.title] = task.timeframes
+    })
+
+    return tasksByTime
+}
+
+async function fetchTasks() {
+    const res = await fetch('./js/data.json')
+    const tasks = await res.json()
+    return tasks
+}
+
+
+function buttonActive(event) {
+    buttons.forEach((element) => {
+        element.classList.remove('active')
+    })
+    event.target.classList.add('active')
+    timeframe = event.target.innerText.toLowerCase()
+    updateBoxes(timeframe)
+}
+
+function updateBoxes(timeframe) {
+    secBox.forEach((box) => {
+        updateBox(box, timeframe)
+    })
+}
+
+function updateBox(box, timeframe) {
+    const title = box.querySelector('.stats-box-h2').innerText
+    const current = edata[title][timeframe].current
+    const previous = edata[title][timeframe].previous
+
+    const hoursEl = box.querySelector('.stats-box-hours-p')
+    hoursEl.innerText = `${current}hrs`
+    const msgEl = box.querySelector('.stats-box-des-p')
+    msgEl.innerText = `${upDtxt[timeframe]} - ${previous}hrs`
+}
+
+function renderData(element, timeframe) {
+    let title = element.title
+    let current = element.timeframes[timeframe].current
+    let previous = element.timeframes[timeframe].previous
+
+    return `
+`
 }
